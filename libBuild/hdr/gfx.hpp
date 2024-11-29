34cf4aef1d5a07841b7831ff58c42205d0a7ec8b
RealFaceCode <kevin-ernst@xyz.de>
2024-11-29 15:47:09 +0100

    Enhance texture binding functionality by adding SetTextureBindingMode and GetTextureBindingMode functions, and update Init to accept a TextureBindingMode parameter.

--- libBuild/hdr/gfx.hpp
+++ libBuild/hdr/gfx.hpp
@@ -6,11 +6,13 @@ typedef void *(*loadproc)(const char *name);
 
 namespace gfx
 {
-    GFX_API bool Init(loadproc);
+    GFX_API bool Init(loadproc, TextureBindingMode = TextureBindingMode::BIND);
     GFX_API void DeInit();
     GFX_API void Clear();
     GFX_API void PrintVRVOpenGL();
     GFX_API void PrintSupportedOpenGLFeatures();
     GFX_API bool CheckSupoortedOpenGLFeature(const char*);
     GFX_API void WireFrameMode(bool);
+    GFX_API void SetTextureBindingMode(TextureBindingMode);
+    GFX_API TextureBindingMode GetTextureBindingMode();
 }
--- libBuild/src/gfx.cpp
+++ libBuild/src/gfx.cpp
@@ -4,9 +4,13 @@
 
 namespace gfx
 {
-    bool Init(loadproc getProcAddress)
+    static TextureBindingMode bindingMode = TextureBindingMode::NONE;
+
+    bool Init(loadproc getProcAddress, TextureBindingMode mode)
     {
         auto load = gladLoadGLLoader(getProcAddress);
+        if (load)
+            SetTextureBindingMode(mode);
         return load;
     }
 
@@ -58,5 +62,29 @@ namespace gfx
             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         else
             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
+
+    void SetTextureBindingMode(TextureBindingMode mode)
+    {
+        if(mode == TextureBindingMode::BINDLESS)
+        {
+            if(CheckSupoortedOpenGLFeature(BINDLES_TEXTURE))
+            {
+                bindingMode = mode;
+                lc::Log<"GFX BUILD">("DEBUG", "Bindless texture mode enabled");
+            }
+            else
+                lc::Log<"GFX BUILD">("WARNING", "Bindless texture mode not supported! Falling back to bind mode");
+        }
+        else
+        {
+            bindingMode = mode;
+            lc::Log<"GFX BUILD">("DEBUG", "Bind texture mode enabled");
+        }
+    }
+
+    TextureBindingMode GetTextureBindingMode()
+    {
+        return bindingMode;
     }
 }
