95195cbf990e103098905ab2dc25720579584e89
RealFaceCode <kevin-ernst@xyz.de>
2024-11-29 18:39:30 +0100

    Add UBO class for uniform buffer object management

---
+++ libBuild/hdr/buffers/ubo.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "defines.hpp"
+#include <cstdint>
+#include "idp.hpp"
+
+namespace gfx
+{
+    class GFX_API UBO
+    {
+    public:
+        UBO();
+        UBO(uint64_t size, int mode);
+        UBO(uint64_t size, void* data, int mode);
+        ~UBO();
+
+        void map(uint64_t offset, int flag, void* data);
+
+        uint32_t getID() const;
+        uint64_t getBlockID() const;
+
+    private:
+        void create(uint64_t size, void* data, int mode);
+        uint32_t buffer;
+        eutil::idp::ID id;
+        uint64_t size;
+    };
+}
---
+++ libBuild/src/buffers/ubo.cpp
@@ -0,0 +1,54 @@
+#include "buffers/ubo.hpp"
+#include <glad/glad.h>
+#include "gfx.hpp"
+
+namespace gfx
+{
+    static eutil::idp::IDPool idPool(0, GetMaxUBOSiumultaneousBindings());
+
+    UBO::UBO()
+    : buffer(0), id(), size(0)
+    {}
+
+    UBO::UBO(uint64_t size, int mode)
+    : size(size)
+    {
+        create(size, nullptr, mode);
+    }
+
+    UBO::UBO(uint64_t size, void* data, int mode)
+    {
+        create(size, data, mode);
+    }
+
+    UBO::~UBO()
+    {
+        glDeleteBuffers(1, &buffer);
+    }
+
+    void UBO::map(uint64_t offset, int flag, void* data)
+    {
+        glBindBufferBase(GL_UNIFORM_BUFFER, id.get(), buffer);
+        uint8_t* map = (uint8_t*)glMapBufferRange(GL_UNIFORM_BUFFER, offset, size, flag);
+        memcpy(map, data, size);
+        glUnmapBuffer(GL_UNIFORM_BUFFER);
+    }
+
+    void UBO::create(uint64_t size, void* data, int mode)
+    {
+        glGenBuffers(1, &buffer);
+        glBindBuffer(GL_UNIFORM_BUFFER, buffer);
+        glBufferData(GL_UNIFORM_BUFFER, size, data, GL_DYNAMIC_DRAW);
+        id = idPool.getID();
+    }
+
+    uint32_t UBO::getID() const
+    {
+        return buffer;
+    }
+
+    uint64_t UBO::getBlockID() const
+    {
+        return id.get();
+    }
+}
